f9f4fa8cff2f48f6fcc5cc3be2e64083e48ef24d
Lauren Oden <laurenoden97@gmail.com>
2024-12-18 18:54:01 -0500

    Initial commit

---
+++ my-vue-app/src/components/common/CustomComponents.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+
+const CustomNavLink = ({ href, className, children }) => {
+    const linkStyles = "text-[15px font-medium text-gray-600 cursor-pointer list-none";
+    return (
+        <NavLink 
+            to={href}
+            className={({ isActive }) => isActive ? `${className} ${linkStyles} text-primary-green` : `${className} ${linkStyles}`}
+        >
+            {children}
+        </NavLink>
+    );
+};
+
+const CustomLink = ({ className, children }) => {
+    const linkStyles = "text-[15px font-medium text-gray-600 cursor-pointer list-none";
+    return (<NavLink className={`${className} ${linkStyles}`}>{children}</NavLink>);
+};
+
+
+const Badges = ({ color, children }) => {
+    return (
+        <div className={`w-[18px] h-[18px] ${color} rounded-full text-[12px] flex justify-center text-white`}>
+            {children}
+        </div>
+    )
+}
+
+export { CustomLink, Badges, CustomNavLink };
+
+CustomNavLink.propTypes = {
+    href: PropTypes.isRequired,
+    className: PropTypes.isRequired,
+    children: PropTypes.isRequired
+};
+
+CustomLink.propTypes = {
+    className: PropTypes.isRequired,
+    children: PropTypes.isRequired
+};
+
+Badges.propTypes = {
+    color: PropTypes.isRequired,
+    children: PropTypes.isRequired
+};
---
+++ my-vue-app/src/router/index.js
@@ -0,0 +1,2 @@
+export { Layout } from "../components/common/Layout";
+export { Home } from "../screen/home/Home";
